d9762ef10e42622b65e51f05883e0ccbc764ee83
poinner02 <55753056+poinner02@users.noreply.github.com>
2022-11-04 21:46:38 +0800

    test git 1

--- gulimall-auth-service/src/main/java/com/merchen/gulimall/auth/GlimallAuthServiceApplication.java
+++ gulimall-auth-service/src/main/java/com/merchen/gulimall/auth/GlimallAuthServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GlimallAuthServiceApplication {
-    //test git
+    //test git  2
 
     public static void main(String[] args) {
         SpringApplication.run(GlimallAuthServiceApplication.class, args);
